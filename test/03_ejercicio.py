9d826ca0b6daa21c67a2044fec160b08d065f64c
gerlier <gerlifull@gmail.com>
2022-05-09 23:38:20 -0500

    complete

--- test/03_ejercicio.py
+++ test/03_ejercicio.py
@@ -17,7 +17,9 @@ driver.get("https://laboratorio.qaminds.com/")
 time.sleep(5)
 
 # Test Logic
-my_account: WebElement = driver.find_element(By.LINK_TEXT, "My Account")
+time.sleep(5)
+
+my_account: WebElement = driver.find_element(By.LINK_TEXT, "My Account")#or con xpath other exaple for looging the other acount is: //footer//a[normalize-space(.)='My Account']
 assert my_account.is_displayed(), "my account is not visible"
 my_account.click()
 
@@ -30,17 +32,20 @@ assert correo.is_displayed(), "correo is not visible"
 correo.clear()
 correo.send_keys("casa")
 
+password : WebElement = driver.find_element(By.ID, "input-password")
+assert password.is_displayed(), "password is not visible"
+password.clear()
+password.send_keys("casas")
+
+
+
 login_btn = WebElement = driver.find_element(By.XPATH, '//input[@value="Login"]')
 assert login_btn.is_displayed(), "login is not visible"
 login_btn.click()
 
-clase_error : WebElement = driver.find_element(By.XPATH, "alert alert-danger alert-dismissible")
+clase_error : WebElement = driver.find_element(By.XPATH,"//*[contains(@class,'fa-exclamation-circle')]")
 assert clase_error.is_displayed(),"no hay error"
-
-clase_error.text == " Warning: No match for E-Mail Address and/or Password.", "no coincide el texto de error"
-
-
-
+assert clase_error.text == " Warning: No match for E-Mail Address and/or Password.", "no coincide el texto de error"
 
 
 # Close browser
