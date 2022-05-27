9aa536a44b13f6e381a85265de53dd83a93956ae
gerlier <gerlifull@gmail.com>
2022-05-26 22:05:36 -0500

    succes home

--- test/test_final_project_home.py
+++ test/test_final_project_home.py
@@ -8,5 +8,22 @@ class TestLoginPage:
 
     def setup_method(self):
         self.driver: WebDriver = get_driver()
-        self.product_page = ProductPage(self.driver)
-        self.product_page.goto("https://laboratorio.qaminds.com/index.php?route=product/product&product_id=33&search=samsung")
+        self.driver.get(config.get_url())
+        self.home_page = HomePage(self.driver)
+
+    def test_currency_pesos(self):
+        assert self.home_page.get_currency() == "$", "currency is not in pesos"
+
+    def test_currency_euro(self):
+        self.home_page.set_currency("EUR")
+        assert "€" == self.home_page.get_currency(), "there was not selected currency euro"
+
+    def test_currency_pound(self):
+        self.home_page.set_currency("GBP")
+        assert "£" == self.home_page.get_currency(), "there was not selected currency Pound Sterling"
+      
+    def teardown_method(self):
+        if self.driver:
+            self.driver.quit()    
+        
+
